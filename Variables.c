f4a07668ad41b2472018799ca4c9f7b0b5488cdd
Paty Zaragoza <patypalma878@gmail.com>
2024-10-18 20:12:13 -0600

    Archivos C

---
+++ Variables.c
@@ -0,0 +1,15 @@
+/* Multiplica dos números enteros y muestra el
+resultado por pantalla */
+
+#include <stdio.h>
+int main( )
+{
+int multiplicador; /*se define multiplicador como un entero */
+int multiplicando; /*se define multiplicando como un entero */
+int res; /*se define resultado como un entero*/
+multiplicador = 2500; /*se asignan valores*/
+multiplicando=320;
+res=multiplicador*multiplicando;
+printf("Resultado =%d\n",res); /*se muestra el resultado */
+return 0;
+}
